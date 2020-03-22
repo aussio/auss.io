29b32e7b251ead174daf2053f10717f3f6bd6c7a
Austin Curtis <austin.curtis.mail@gmail.com>
2020-03-22 18:59:42 -0400

    Debug to True in Production

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2)55z!fw11k*ppvex-uj#9oksykg+vrj)b*ace&eseth3lcj9*'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
