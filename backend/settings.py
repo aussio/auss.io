4538fd1337198e7af0134598674ceff1591cae32
Austin Curtis <austin.curtis.mail@gmail.com>
2020-03-22 19:30:18 -0400

    Add Django admin static files to build/static directory for Heroku.

--- backend/settings.py
+++ backend/settings.py
@@ -139,23 +139,7 @@ STATICFILES_DIRS = []
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 try:
     from .local_settings import *
