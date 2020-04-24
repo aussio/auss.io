b7589589db16d16c7f207d25098d6ffcefd686ea
Austin Curtis <austin.curtis.mail@gmail.com>
2020-04-24 14:57:01 -0400

    Landing page rework progress.

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,6 +1,13 @@
-export const veryVeryLight = 'rgba(152, 46, 240, 5%)'; // Currently used for the <pre> tags in blog posts
-export const veryLight = 'rgba(152, 46, 240, 7%)';
-export const normal = 'rgba(152, 46, 240, 65%)';
-export const dark = 'rgba(152, 46, 240, 85%)';
+const BASE_COLOR = '122, 61, 201';
+export const veryVeryLight = `rgba(${BASE_COLOR}, 5%)`; // Currently used for the <pre> tags in blog posts
+export const veryLight = `rgba(${BASE_COLOR}, 15%)`;
+export const light = `rgba(${BASE_COLOR}, 35%)`;
+export const normal = `rgba(${BASE_COLOR}, 65%)`;
+export const dark = `rgba(${BASE_COLOR}, 85%)`;
 
 export const text = 'rgb(46, 46, 46)';
+
+export const red = 'rgb(209, 73, 91)';
+export const orange = 'rgb(216, 69, 6)';
+export const charcoal = 'rgb(30, 57, 71)';
+export const blue = 'rgb(46, 107, 223)';
