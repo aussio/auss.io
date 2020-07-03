8ded21c07df98a54ae69662f033118b112bcd491
Austin Curtis <austin.curtis.mail@gmail.com>
2020-07-02 19:01:40 -0500

    Text content within LandingPage.

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,8 +1,11 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/accessible-emoji */
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import Gallery from 'react-photo-gallery';
+import { Link } from 'react-router-dom';
+import ExternalLink from './lib/ExternalLink';
 import * as COLORS from './theme/colors';
 
 export default function LandingPage() {
@@ -84,47 +87,96 @@ export default function LandingPage() {
         </div>
       </div>
 
-      <Section color="white">
-        A couple of sentences about me. Welcome to the site, it's about my work, thoughts, and feelings.
-      </Section>
-      <Section color={COLORS.normal}>
-        <SectionHeader>My Work</SectionHeader>
-        <p
-          css={{
-            fontWeight: 400,
-            color: 'white',
-          }}
-        >
-          Coming Soon - I'll put examples of projects that I'm proud of and links to more information.
+      <Section
+        color="white"
+        css={{
+          '*': {
+            margin: 0,
+          },
+        }}
+      >
+        <p>
+          Welcome to information about my work, thoughts, and feelings. I plan on this website becoming like a living journal of my experiences - more to come!
         </p>
       </Section>
-      <Section color={COLORS.red}>
+      <Section
+        color={COLORS.normal}
+        css={{
+          color: 'white',
+        }}
+      >
+        <SectionHeader>
+          My Work
+        </SectionHeader>
+        {/* <strong>Backend</strong> */}
+        <p>
+          I&apos;ve spent my professional career so far as a backend software developer working on making WP Engine&apos;s platform run faster, bigger, and more efficiently. These projects include development of new API&apos;s, improving the testability and test coverage of large monoliths, refactoring large monoliths into distinct services, and performance monitoring.
+        </p>
+        {/* <strong>Frontend</strong> */}
+        <p>
+          Of late I&apos;ve spent my personal time learning React and CSS to unlock the power of creating user interfaces outside of a terminal.
+        </p>
+        <p
+          css={{
+            marginTop: '3rem',
+            fontWeight: 600,
+          }}
+        >
+          <Link
+            to="/resume"
+            css={{
+              color: 'white',
+            }}
+          >
+            See more details within my long form resume
+          </Link>
+        </p>
+      </Section>
+      <Section
+        color={COLORS.red}
+        css={{
+          color: 'white',
+          '*': {
+            color: 'white',
+          },
+        }}
+      >
         <SectionHeader>Professional Interests</SectionHeader>
-        <p
-          css={{
-            fontWeight: 400,
-            color: 'white',
-          }}
-        >
-          Coming Soon - I'll put blurbs of areas of interest that I'd most love to work on and possibly links to blog posts about them.
+        <strong>Macro</strong>
+        <p>
+          I quit my previous job with the goal of finding work that more directly contributes to improving the world. I am impressed and motivated by organizations like
+          {' '}
+          <ExternalLink to="https://www.openphilanthropy.org/">Open Philanthropy</ExternalLink>
+          {' '} and {' '}
+          <ExternalLink to="https://80000hours.org/key-ideas/">80,000 Hours</ExternalLink>
+          {' '} and I would really like the opportunity to spend my 2000 hours per year at work both making money and making an impact. Topics like Education, Factory Farming, and Internet Privacy really interest me.
+        </p>
+        <strong>Micro</strong>
+        <p>
+          Throughout my career, one of the most rewarding areas for me has been mentoring and continuing education. I&apos;ve personally mentored 4 people into getting their first jobs in Tech through Technical Support, 3 people into getting their first Software Engineering jobs, and dozens of others in first learning to code. So far no project launch or code deploy has been so fulfilling. I absolutely love inspiring others to achieve their dreams and break out of whatever cognitive boxes they may been stuck within.
         </p>
       </Section>
-      <Section color={COLORS.charcoal}>
-        <SectionHeader>Hobbies</SectionHeader>
-        <p
-          css={{
-            fontWeight: 400,
+      <Section
+        color={COLORS.charcoal}
+        css={{
+          '*': {
             color: 'white',
-          }}
-        >
-          Coming Soon - I'll have blurbs about my main hobbies and links to related blog posts or website sections.
+          },
+        }}
+      >
+        <SectionHeader>Hobbies</SectionHeader>
+        <p>
+          Outside of work I spend a lot of time with <ExternalLink to="https://www.instagram.com/caitlin.nicole.onaroll/">my beautiful fiancè Caitlin</ExternalLink> and <ExternalLink to="https://www.instagram.com/austin.shepsky/">our dogs Jack and Penny</ExternalLink>. The four of us love to travel and have just recently finished a 10-month RV trip around the United States.
+        </p>
+        <p>
+          I also enjoy playing a variety of video games (recents being Hollow Knight, Breath of the Wild, and League of Legends), lifting weights, and random arts & craft projects.
         </p>
       </Section>
     </div>
   );
 }
 
-function Section({ color, children }) {
+function Section({ color, children, className }) {
   return (
     <section
       css={{
@@ -132,24 +184,26 @@ function Section({ color, children }) {
         textAlign: 'center',
         padding: '5rem',
       }}
+      className={className}
     >
       {children}
     </section>
   );
 }
 
-function SectionHeader({ children }) {
+function SectionHeader({ children, className }) {
   return (
     <h1
       css={{
         margin: 0,
-        padding: '.5em',
+        padding: '0 2rem 2rem 2rem',
         color: 'white',
         '@media(max-width: 450px)': {
           padding: 0,
           fontSize: '3rem',
         },
       }}
+      className={className}
     >
       {children}
     </h1>
